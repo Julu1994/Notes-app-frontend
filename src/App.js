68c2418f3facbc90b8cf114c89c9b6ef65c63a5a
jewel <mrjewel837@gmail.com>
2023-02-25 16:05:22 +0100

    changed the BrowserRoter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
 const App = () => {
     return (
-        <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/auth/signup" element={<SignupPage />} />
-            <Route element={<ProtectedRoutes />}>
-                <Route path="/home" element={<HomePage />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/home" replace />} />
-        </Routes>
+        <HashRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/auth/signup" element={<SignupPage />} />
+                <Route element={<ProtectedRoutes />}>
+                    <Route path="/home" element={<HomePage />} />
+                </Route>
+                <Route path="*" element={<Navigate to="/home" replace />} />
+            </Routes>
+        </HashRouter>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
@@ -12,15 +11,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <ContextProvider>
-            <BrowserRouter>
-                <ThemeProvider theme={theme}>
-                    <App />
-                    <Toaster
-                        position="top-right"
-                        toastOptions={{ duration: 3500 }}
-                    />
-                </ThemeProvider>
-            </BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <App />
+                <Toaster
+                    position="top-right"
+                    toastOptions={{ duration: 3500 }}
+                />
+            </ThemeProvider>
         </ContextProvider>
     </React.StrictMode>
 );
